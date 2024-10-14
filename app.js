97e1e991f992eae0ccf0d4b91a4161f493f4a4d6
AugustTimes77 <Henrybaer7@gmail.com>
2024-10-14 17:44:12 -0400

    Initial commit

---
+++ app.js
@@ -0,0 +1,33 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  // Path to your index.html file
+  const filePath = path.join(__dirname, 'Website', 'index.html');
+
+  fs.readFile(filePath, (err, content) => {
+    if (err) {
+      if (err.code === 'ENOENT') {
+        // File not found
+        res.writeHead(404);
+        res.end('404 Not Found');
+      } else {
+        // Server error
+        res.writeHead(500);
+        res.end('500 Internal Server Error');
+      }
+    } else {
+      // Success
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(content, 'utf-8');
+    }
+  });
+});
+
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
